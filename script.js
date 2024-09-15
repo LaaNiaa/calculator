b3846ca8a610b71274b21615aba4ac011ceb27e2
LaaNiaa <laaniaa262626@gmail.com>
2024-09-15 16:57:04 +0200

    modified script.js

--- script.js
+++ script.js
@@ -24,3 +24,25 @@ function input(val) {
 function ac() {
     document.getElementById("result").value = '0';
 }
+
+// function operator(op) {
+//     let num1 = document.getElementById("result").value;
+     
+//     switch (true) {
+//         case op === 'add':
+//             console.log(num1);
+//             input();
+//             console.log(document.getElementById("result").value);
+//             break;
+//     }
+// }
+
+function calculate() {
+
+}
+
+/**
+ * type first number -> after clicking math operator change button color to white,
+ * but the first number stays on result display -> after typing next number clear and save in memory the first number,
+ * the operator button goes back to orange color -> after clicking equals display the result
+ */
